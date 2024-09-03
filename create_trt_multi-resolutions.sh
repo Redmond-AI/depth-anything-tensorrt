95f41667069da08a30e2b445465faa98bbf4684a
Redmond-AI <126352754+Redmond-AI@users.noreply.github.com>
2024-09-03 17:08:18 -0400

    Update create_trt_multi-resolutions.sh

--- create_trt_multi-resolutions.sh
+++ create_trt_multi-resolutions.sh
@@ -9,11 +9,13 @@ iterations=10
 for i in $(seq 1 $iterations); do
     echo "Running iteration $i with size $size"
 
+    mkdir depth_anything_v2_vitl_4090_${size}
+
     # Export ONNX
-    PYTHONPATH=. python tools/export_onnx.py --checkpoint /app/myrepo/depth-anything-tensorrt/third_party/depth_anything_v2/depth_anything_v2/checkpoints/depth_anything_v2_vitl.pth --onnx depth_anything_v2_vitl_4090_${size}.onnx --input_size ${size} --encoder vitl 
+    PYTHONPATH=. python tools/export_onnx.py --checkpoint /app/myrepo/depth-anything-tensorrt/third_party/depth_anything_v2/depth_anything_v2/checkpoints/depth_anything_v2_vitl.pth --onnx depth_anything_v2_vitl_4090_${size}/depth_anything_v2_vitl_4090_${size}.onnx --input_size ${size} --encoder vitl 
 
     # Convert ONNX to TRT
-    PYTHONPATH=. python trt_build_engine.py --onnx depth_anything_v2_vitl_4090_${size}.onnx --engine depth_anything_v2_vitl_4090_${size}.trt --fp16 --workspace 20
+    PYTHONPATH=. python trt_build_engine.py --onnx depth_anything_v2_vitl_4090_${size}/depth_anything_v2_vitl_4090_${size}.onnx --engine depth_anything_v2_vitl_4090_${size}.trt --fp16 --workspace 20
 
     # Run inference
     git pull
@@ -22,6 +24,7 @@ for i in $(seq 1 $iterations); do
     # Delete the .trt file
     rm depth_anything_v2_vitl_4090_${size}.trt
     echo "Deleted depth_anything_v2_vitl_4090_${size}.trt"
+    rm -r depth_anything_v2_vitl_4090_${size}
     rm depth_anything_v2_vitl_4090_${size}.onnx
     echo "Deleted depth_anything_v2_vitl_4090_${size}.onnx"
     # Increment size by 14
