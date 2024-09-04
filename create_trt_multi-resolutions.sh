043e9c08ebad88904f190f6114b62992c888bfd2
Redmond-AI <126352754+Redmond-AI@users.noreply.github.com>
2024-09-03 20:58:36 -0400

    Update create_trt_multi-resolutions.sh

--- create_trt_multi-resolutions.sh
+++ create_trt_multi-resolutions.sh
@@ -15,7 +15,7 @@ for i in $(seq 1 $iterations); do
     PYTHONPATH=. python tools/export_onnx.py --checkpoint /app/myrepo/depth-anything-tensorrt/third_party/depth_anything_v2/depth_anything_v2/checkpoints/depth_anything_v2_vitg.pth --onnx depth_anything_v2_vitg_4090_${size}/depth_anything_v2_vitg_4090_${size}.onnx --input_size ${size} --encoder vitg 
 
     # Convert ONNX to TRT
-    PYTHONPATH=. python trt_build_engine.py --onnx depth_anything_v2_vitg_4090_${size}/depth_anything_v2_vitg_4090_${size}.onnx --engine depth_anything_v2_vitg_4090_${size}.trt --fp16 --workspace 20
+    PYTHONPATH=. python trt_build_engine.py --onnx depth_anything_v2_vitg_4090_${size}/depth_anything_v2_vitg_4090_${size}.onnx --engine depth_anything_v2_vitg_4090_${size}.trt --tf32 --workspace 20
 
     # Run inference
     git pull
